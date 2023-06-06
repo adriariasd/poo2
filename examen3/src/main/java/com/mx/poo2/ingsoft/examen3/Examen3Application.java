987e3fc95c8d45b17fc6f391a6b1c3258edb1dde
Adrian Arias <adriariasd@gmail.com>
2023-06-05 18:12:52 -0600

    se anadio todo el segundo proyecto

--- examen3/src/main/java/com/mx/poo2/ingsoft/examen3/Examen3Application.java
+++ examen3/src/main/java/com/mx/poo2/ingsoft/examen3/Examen3Application.java
@@ -2,8 +2,10 @@ package com.mx.poo2.ingsoft.examen3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.mx.poo2.ingsoft.examen3.modelo")
 public class Examen3Application {
 
 	public static void main(String[] args) {
